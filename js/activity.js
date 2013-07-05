01a766502f7d26a022c2317c4512608ac94899f2
Manuel Quiñones <manuq@laptop.org>
2013-07-05 15:35:42 -0300

    Create the table dinamically
    Add mustache <https://github.com/janl/mustache.js> as template system.

--- js/activity.js
+++ js/activity.js
@@ -1,6 +1,7 @@
 define(function (require) {
     var activity = require("sugar-web/activity/activity");
     var icon = require("sugar-web/graphics/icon");
+    var mustache = require("mustache");
 
     // Manipulate the DOM only when it is ready.
     require(['domReady!'], function (doc) {
@@ -20,6 +21,38 @@ define(function (require) {
             activity.close();
         });
 
+        // Create table.
+        var tableElem = document.getElementById("buttons-table");
+
+        var tableTemplate =
+            '<tbody>' +
+            '{{#rows}}' +
+              '<tr>' +
+                '{{#.}}' +
+                '<td>' +
+                  '<button id="{{id}}">{{suit}}</button>' +
+                '</td>' +
+                '{{/.}}' +
+              '</tr>' +
+            '{{/rows}}' +
+            '</tbody>';
+
+        tableData = {
+            "rows": [
+                [{"id": 1, "suit": 1}, {"id": 2, "suit": 1},
+                 {"id": 3, "suit": 1}, {"id": 4, "suit": 1}],
+                [{"id": 5, "suit": 1}, {"id": 6, "suit": 1},
+                 {"id": 7, "suit": 1}, {"id": 8, "suit": 1}],
+                [{"id": 9, "suit": 2}, {"id": 10, "suit": 2},
+                 {"id": 11, "suit": 2}, {"id": 12, "suit": 2}],
+                [{"id": 13, "suit": 2}, {"id": 14, "suit": 2},
+                 {"id": 15, "suit": 2}, {"id": 16, "suit": 2}]
+            ]
+        };
+
+        tableElem.innerHTML = mustache.render(tableTemplate, tableData);
+
+        // Add callback to click events of table buttons.
         var buttonPressed = function (e) {
             console.log(this.getAttribute('id'));
         };
