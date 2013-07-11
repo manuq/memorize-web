d7215688c9df079fc42637eae00290d004f0d7e0
Manuel Quiñones <manuq@laptop.org>
2013-07-11 14:56:16 -0300

    Update to latest sugar-web

--- js/activity.js
+++ js/activity.js
@@ -1,6 +1,5 @@
 define(function (require) {
     var activity = require("sugar-web/activity/activity");
-    var icon = require("sugar-web/graphics/icon");
 
     var model = require("activity/model");
     var view = require("activity/view");
@@ -11,18 +10,6 @@ define(function (require) {
 
         // Initialize the activity.
         activity.setup();
-
-        // Colorize the activity icon.
-        var activityButton = document.getElementById("activity-button");
-        activity.getXOColor(function (error, colors) {
-            icon.colorize(activityButton, colors);
-        });
-
-        // Make the activity stop with the stop button.
-        var stopButton = document.getElementById("stop-button");
-        stopButton.addEventListener('click', function (e) {
-            activity.close();
-        });
 
         // Example game.
         var cardsSet = [
